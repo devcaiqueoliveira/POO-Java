a57c773983b64af3640bd02c507601d251742d83
Caique <devcaiqueoliveira@gmail.com>
2025-01-17 11:06:34 -0300

    Aula de criacao de classes e instanciar objetos

--- Aula02/src/Main.java
+++ Aula02/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        Caneta c1 = new Caneta;
+        Caneta c1 = new Caneta();
+        c1.cor = "Azul";
+        c1.ponta = 0.5f;
+        c1.tampar();
+        //c1.status();
+        c1.rabiscar();
+
+        Caneta c2 = new Caneta();
+        c2.modelo = "Bahas";
+        c2.cor = "Verde";
+        c2.destampar();
+        c2.rabiscar();
     }
 }
