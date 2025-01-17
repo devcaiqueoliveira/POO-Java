364b8d4a6aa468bc2758d9921f71b8f909e61905
Caique <devcaiqueoliveira@gmail.com>
2025-01-17 10:37:40 -0300

    Muita coisa

---
+++ Aula02/src/Caneta.java
@@ -0,0 +1,11 @@
+public class Caneta {
+        String modelo;
+        String cor;
+        float ponta;
+        int carga;
+        boolean tampada;
+
+        void status() {
+            System.out.println("Modelo: " + this.modelo);
+        }
+}
