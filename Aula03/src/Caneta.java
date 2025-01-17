8157b711faec770ff7d3f12390c8855ded9c1cce
Caique Oliveira <devcaiqueoliveira@gmail.com>
2025-01-17 19:38:59 -0300

    .

--- Aula03/src/Caneta.java
+++ Aula03/src/Caneta.java
@@ -1,11 +1,11 @@
 public class Caneta {
-    String modelo;
-    String cor;
-    float ponta;
-    int carga;
-    boolean tampada;
+    public String modelo;
+    public String cor;
+    private float ponta;
+    protected int carga;
+    protected boolean tampada;
 
-    void status() {
+    public void status() {
         System.out.println("Modelo: " + this.modelo);
         System.out.println("Uma caneta " + this.cor);
         System.out.println("Ponta: " + this.ponta);
@@ -13,7 +13,7 @@ public class Caneta {
         System.out.println("Esta tampada? " + this.tampada);
     }
 
-    void rabiscar() {
+    public void rabiscar() {
         if (this.tampada == true) {
             System.out.println("ERRO! NÃO POSSO RABISCAR");
         } else {
@@ -22,11 +22,11 @@ public class Caneta {
 
     }
 
-    void tampar() {
+    protected void tampar() {
         this.tampada = true;
     }
 
-    void destampar() {
+    protected void destampar() {
         this.tampada = false;
     }
 }
