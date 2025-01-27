3bbd6c0825f1b5b57617185ce528e2d87b67aea8
Caique Oliveira <devcaiqueoliveira@gmail.com>
2025-01-27 10:40:16 -0300

    Aula07
    Bom demaaaais

---
+++ Aula07/src/Lutador.java
@@ -0,0 +1,131 @@
+public class Lutador {
+    private String nome, categoria, nacionalidade;
+    private int idade, vitorias, derrotas, empates;
+    private float altura, peso;
+
+    public Lutador (String no, String na, int id, float al, float pe, int vi, int de, int em) {
+        this.nome = no;
+        this.nacionalidade = na;
+        this.idade = id;
+        this.altura = al;
+        this.setPeso(pe);
+        this.vitorias = vi;
+        this.derrotas = de;
+        this.empates = em;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String no) {
+        this.nome = no;
+    }
+
+    public float getPeso() {
+        return this.peso;
+    }
+
+    public void setPeso(float pe) {
+        this.peso = pe;
+        setCategoria();
+    }
+
+    public String getCategoria() {
+        return this.categoria;
+    }
+
+    public void setCategoria() {
+        if (this.getPeso() < 52.2) {
+            this.categoria = "Inválido";
+        } else if (this.getPeso() <= 70.3) {
+            this.categoria = "Peso Leve";
+        } else if (this.getPeso() <= 83.9) {
+            this.categoria = "Media";
+        } else if (this.getPeso() <= 120.2) {
+            this.categoria = "Pesada";
+        } else {
+            this.categoria = "Inválido";
+        }
+    }
+
+    public String getNacionalidade() {
+        return this.nacionalidade;
+    }
+
+    public void setNacionalidade(String na) {
+        this.nacionalidade = na;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(int id) {
+        this.idade = id;
+    }
+
+    public float getAltura() {
+        return this.altura;
+    }
+
+    public void setAltura(float al) {
+        this.altura = al;
+    }
+
+    public int getVitorias() {
+        return this.vitorias;
+    }
+
+    public void setVitorias(int vi) {
+        this.vitorias = vi;
+    }
+
+    public int getDerrotas() {
+        return this.derrotas;
+    }
+
+    public void setDerrotas(int de) {
+        this.derrotas = de;
+    }
+
+    public int getEmpates() {
+        return this.empates;
+    }
+
+    public void setEmpates(int em) {
+        this.empates = em;
+    }
+
+    public void apresentar() {
+        System.out.println("--------------------------");
+        System.out.println("Lutador: " + this.getNome());
+        System.out.println("Origem: " + this.getNacionalidade());
+        System.out.println(this.getIdade() + " anos");
+        System.out.println(this.getAltura() + "m de altura");
+        System.out.println("Pesando: " + this.getPeso() + "kg");
+        System.out.println("Ganhou: " + this.getVitorias());
+        System.out.println("Perdeu: " + this.getDerrotas());
+        System.out.println("Empatou: " + this.getEmpates());
+    }
+
+    public void status() {
+        System.out.println(this.getNome() + " é um " + this.getCategoria());
+        System.out.println("Ganhou " + this.getVitorias() + " vezes");
+        System.out.println("Perdeu " + this.getDerrotas() + " vezes");
+        System.out.println("Empatou " + this.getEmpates() + " vezes");
+    }
+
+    public void ganharLuta() {
+        this.setVitorias(this.getVitorias() + 1);
+    }
+
+    public void perderLuta() {
+        this.setDerrotas(this.getDerrotas() + 1);
+    }
+
+    public void empatarLuta() {
+        this.setEmpates(this.getEmpates() + 1);
+    }
+
+}
---
+++ ExercicioGPT4/src/Animal.java
@@ -0,0 +1,7 @@
+public interface Animal {
+    String fazerSom();
+    String getTipoAnimal();
+
+
+
+}
