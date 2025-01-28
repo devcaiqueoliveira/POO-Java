f292d0e5accbeb5bdd5d586bcd59ed2df8af31f8
Caique <devcaiqueoliveira@gmail.com>
2025-01-28 10:43:22 -0300

    Update Main.java

--- Aula07/src/Main.java
+++ Aula07/src/Main.java
@@ -13,6 +13,8 @@ public class Main {
 
         l[5] = new Lutador("Nerdaart", "EUA", 30, 1.81f, 105.7f, 12, 2, 4);
 
-        l[3].status();
+        Luta UEC01 = new Luta();
+        UEC01.marcarLuta(l[0], l[1]);
+        UEC01.lutar();
     }
 }
