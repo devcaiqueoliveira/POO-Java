ccf28b39a5a18a660104603947814fa020f5f086
Caique Oliveira <devcaiqueoliveira@gmail.com>
2025-01-30 19:35:25 -0300

    aual11

---
+++ Aula11/src/Bolsista.java
@@ -0,0 +1,19 @@
+public class Bolsista extends Aluno{
+    private int bolsa;
+
+    public void RenovarBolsa() {
+
+    }
+
+    @Override
+    public void PagarMensalidade() {
+
+    }
+
+    public int getBolsa() {
+        return this.bolsa;
+    }
+    public void setBolsa(int bolsa) {
+        this.bolsa = bolsa;
+    }
+}
--- Aula11/src/Pessoa.java
+++ Aula11/src/Pessoa.java
@@ -5,4 +5,34 @@ public abstract class Pessoa {
     public final void  fazerAniv() {
 
     }
+
+    public String getNome() {
+        return this.nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return this.sexo;
+    }
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", sexo='" + sexo + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
 }
